9f14cdddefe224a828010b675a0a4ebfec81052c
Myles Grant <myles@mylesgrant.com>
2013-03-24 16:03:54 -0700

    Initial code for connecting to the socket

---
+++ adsb.js
@@ -0,0 +1,16 @@
+var net = require('net');
+
+var host = '192.168.1.144';
+var port = 8080;
+
+var client = net.connect({host: host, port: port}, function() { //'connect' listener
+	console.log('client connected');
+});
+
+client.on('data', function(data) {
+	console.log(data.toString());
+});
+
+client.on('end', function() {
+	console.log('client disconnected');
+});
