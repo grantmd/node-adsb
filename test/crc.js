fc3d072488ba83e0e849bb0eebbec10f3fc1f604
Myles Grant <myles@mylesgrant.com>
2013-03-29 21:30:33 -0700

    Tests for CRC checks

---
+++ test/crc.js
@@ -0,0 +1,25 @@
+//
+// Tests for CRC checks
+//
+
+var adsb = require('../lib/adsb.js'),
+	assert = require('assert'),
+	should = require('should');
+
+describe('crc', function(){
+	describe('fails', function(){
+		it('should fail the crc check', function(){
+			var packet = adsb.decodePacket('*7f4d2023587f345e35837e2218b2;');
+			packet.should.include({crc_ok: false});
+			packet.should.include({crc: 0x587f34});
+		});
+	});
+
+	describe('passes', function(){
+		it('should pass the crc check', function(){
+			var packet = adsb.decodePacket('*8f4d2023587f345e35837e2218b2;');
+			packet.should.include({crc_ok: true});
+			packet.should.include({crc: 0x2218b2});
+		});
+	});
+});
