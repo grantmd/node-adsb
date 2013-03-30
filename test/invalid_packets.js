7f9733387f5ffbde3808d602ed920fa53f701a54
Myles Grant <myles@mylesgrant.com>
2013-03-29 21:20:26 -0700

    Test for the keys on the valid message

--- test/invalid_packets.js
+++ test/invalid_packets.js
@@ -1,3 +1,7 @@
+//
+// Tests for packet validity. Does not test anything about the decode process, just that we can detect invalid packets that fail before we try and decode them.
+//
+
 var adsb = require('../lib/adsb.js'),
 	assert = require('assert'),
 	should = require('should');
@@ -22,6 +26,12 @@ describe('packets', function(){
 			adsb.decodePacket('*8f4d2023587f345e35837e2218b21;').should.equal(adsb.ERR_PACKET_TOO_LONG);
 
 			adsb.decodePacket('*8f4d2023587f345e35837e2218b2;').should.not.equal(adsb.ERR_PACKET_TOO_LONG);
+		});
+	});
+
+	describe('are not invalid', function(){
+		it('should look like a packet', function(){
+			adsb.decodePacket('*8f4d2023587f345e35837e2218b2;').should.have.keys(['binary','bits','type','crc_ok','crc','errorbit','icao','phase_corrected','ca','metype','mesub','heading_is_valid','heading','aircraft_type','fflag','tflag','raw_latitude','raw_longitude','flight','ew_dir','ew_velocity','ns_dir','ns_velocity','vert_rate_source','vert_rate_sign','vert_rate','velocity','fs','dr','um','identity','altitude','unit']);
 		});
 	});
 });
