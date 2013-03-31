e5ed2ba2906a54ac1da76d597a2a61e2661f593a
Myles Grant <myles@mylesgrant.com>
2013-03-31 12:11:15 -0700

    Add another crc test for checking just the modesChecksum function

--- test/crc.js
+++ test/crc.js
@@ -6,6 +6,28 @@ var adsb = require('../lib/adsb.js'),
 	assert = require('assert'),
 	should = require('should');
 
+describe('checksum', function(){
+	it('should calculate correctly', function(){
+		var bytes = [
+			143,
+			77,
+			32,
+			35,
+			88,
+			127,
+			52,
+			94,
+			53,
+			131,
+			126,
+			34,
+			24,
+			178
+		];
+		adsb.modesChecksum(bytes, bytes.length*8).should.equal(0x2218b2)
+	});
+});
+
 describe('packets', function(){
 	describe('with bad crc', function(){
 		it('should fail the crc check', function(){
