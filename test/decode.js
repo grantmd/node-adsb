d19b517b80c3b69e2c512bdea424c9de7aefa1fe
Myles Grant <myles@mylesgrant.com>
2013-03-31 15:01:54 -0700

    Some ICAO decode tests, including some that we can re-enable once we get CRC fixing working. Yay.

--- test/decode.js
+++ test/decode.js
@@ -30,6 +30,23 @@ describe('packets', function(){
 			adsb.decodePacket('*5d4d20237a55a6;').should.include({ca: 5});
 			adsb.decodePacket('*5f4d20232daf00;').should.include({ca: 7});
 		});
+
+
+		it('should have the correct ICAO code', function(){
+			// Commented out tests are all failing due to bad CRC and us not currently recovering from that. Good!
+
+			adsb.decodePacket('*5d4d20237a55a6;').should.include({icao: [0x4d, 0x20, 0x23]});
+			//adsb.decodePacket('*20000f1f684a6c;').should.include({icao: [0x4d, 0x20, 0x23]});
+			//adsb.decodePacket('*280010248c796b;').should.include({icao: [0x4d, 0x20, 0x23]});
+			adsb.decodePacket('*5d4d20237a55a6;').should.include({icao: [0x4d, 0x20, 0x23]});
+
+			//adsb.decodePacket('*00c18c367cbee4;').should.include({icao: [0xa1, 0xd9, 0x3b]});
+			//adsb.decodePacket('*20000c37971428;').should.include({icao: [0xa1, 0xd9, 0x3b]});
+			//adsb.decodePacket('*00c18c387ceaa5;').should.include({icao: [0xa1, 0xd9, 0x3b]});
+			//adsb.decodePacket('*20000c3868b460;').should.include({icao: [0xa1, 0xd9, 0x3b]});
+
+			adsb.decodePacket('*5da8fd5abb61ee;').should.include({icao: [0xa8, 0xfd, 0x5a]});
+		});
 	});
 
 	describe('that are long', function(){
@@ -58,6 +75,21 @@ describe('packets', function(){
 			// Need more samples in the wild for the CAs
 			adsb.decodePacket('*8d4d2023991094ad487c14fc9e3d;').should.include({ca: 5});
 			adsb.decodePacket('*8f4d2023587f345e35837e2218b2;').should.include({ca: 7});
+		});
+
+		it('should have the correct ICAO code', function(){
+			// Commented out tests are all failing due to bad CRC and us not currently recovering from that. Good!
+			
+			adsb.decodePacket('*8f4d2023587f345e35837e2218b2;').should.include({icao: [0x4d, 0x20, 0x23]});
+			adsb.decodePacket('*8d4d2023991094ad487c14fc9e3d;').should.include({icao: [0x4d, 0x20, 0x23]});
+			adsb.decodePacket('*8d4d202358792453ef858bae7fc9;').should.include({icao: [0x4d, 0x20, 0x23]});
+			adsb.decodePacket('*8f4d20235877d0bc7d99551e27ca;').should.include({icao: [0x4d, 0x20, 0x23]});
+
+			//adsb.decodePacket('*a00007930000000000000068c268;').should.include({icao: [0xa8, 0xfd, 0x5a]});
+
+			adsb.decodePacket('*8fa86e855817a4a338c396d4f13f;').should.include({icao: [0xa8, 0x6e, 0x85]});
+			adsb.decodePacket('*8fa86e859910fd87e0a0065eb299;').should.include({icao: [0xa8, 0x6e, 0x85]});
+			adsb.decodePacket('*8fa86e855819210de2163f721386;').should.include({icao: [0xa8, 0x6e, 0x85]});
 		});
 	});
 });
